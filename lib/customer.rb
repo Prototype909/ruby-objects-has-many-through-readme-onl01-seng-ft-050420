811c5b4319f25b1d9ec8e1e9b49fc2adcff27f6a
Steve Anderson <prototype909@msn.com>
2020-05-20 16:51:48 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,26 @@
 class Customer
-  
+
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialized(name, age)
+    @name = name
+    @age = age
+    @all << self
+  end
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
 end
